e154d3226b14109c8e555088f5fee084b0e3d9ca
lehive <49312027+lehive@users.noreply.github.com>
2023-04-17 12:54:09 +0300

    add initial discussion page along with the specified controller

---
+++ LehikveForum/Controllers/DiscussionForumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiscussionForum.Controllers
+{
+    public class DiscussionForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
