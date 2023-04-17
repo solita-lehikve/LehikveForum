cb162a15cb75856fb969d423f5a3abd320e4a1de
lehive <49312027+lehive@users.noreply.github.com>
2023-04-17 13:30:08 +0300

    add edit topic functionality

--- LehikveForum/Controllers/LehikveForumController.cs
+++ LehikveForum/Controllers/LehikveForumController.cs
@@ -38,5 +38,38 @@ namespace LehikveForum.Controllers
             }
             return View(topic);
         }
+
+
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) {
+                return NotFound();
+            }
+            var topicFromDb = _db.Topics.Find(id);
+            //var topicFromDbFirst = _db.Topics.FirstOrDefault(x => x.Id == id);
+            //var topicFromDbSingle = _db.Topics.SingleOrDefault(x => x.Id == id);
+
+            if (topicFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(topicFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Topic topic)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Topics.Update(topic);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(topic);
+        }
     }
 }
