e0419a6d456015963456e2f1ee528b4d3241bacc
lehive <49312027+lehive@users.noreply.github.com>
2023-04-17 12:48:58 +0300

    add application context

---
+++ DiscussionForum/Data/DiscussionForumContext.cs
@@ -0,0 +1,17 @@
+﻿using DiscussionForum.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscussionForum.Data
+{
+    public class DiscussionForumContext : DbContext
+    {
+        public DiscussionForumContext(DbContextOptions<DiscussionForumContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
